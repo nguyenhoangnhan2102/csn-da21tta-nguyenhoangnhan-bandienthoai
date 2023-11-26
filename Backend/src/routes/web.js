1e3036b9bd2848db0483c3122cf9ca317807e4a8
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2023-11-26 21:07:12 +0700

    26/11/2023

---
+++ Backend/src/routes/web.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+router.get('/nhan', (req, res) => {
+    res.render('sample');
+});
+
+module.exports = router;
