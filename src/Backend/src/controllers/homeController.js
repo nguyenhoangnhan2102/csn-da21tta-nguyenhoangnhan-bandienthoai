238b7a28b889f240dec17eb022a4d8baa5e21a54
nguyenhoangnhan2003 <140692347+nguyenhoangnhan2003@users.noreply.github.com>
2023-12-01 22:03:44 +0700

    01/12/2023

--- src/Backend/src/controllers/homeController.js
+++ src/Backend/src/controllers/homeController.js
@@ -2,10 +2,10 @@ const express = require("express");
 const connection = require("../config/dataBase");
 //const multer = require("multer");
 const getHomePage = async (req, res) => {
-    // const [results, fields] = await connection.execute(
-    //     "SELECT * FROM SANPHAM "
-    // );
-    res.render("home.ejs");
+    const [results, fields] = await connection.execute(
+        "SELECT * FROM SANPHAM "
+    );
+    return res.render("home.ejs", { dataProduct: results });
 };
 
 module.exports = {
--- src/Backend/src/server.js
+++ src/Backend/src/server.js
@@ -3,15 +3,17 @@ const express = require('express');
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('.//routes/web');
+// const cors = require("cors");
+// app.use("/public", express.static(path.join(__dirname, "public")));
 
 const app = express();
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8000;
 const hostname = process.env.HOST_NAME;
 
 configViewEngine(app);
 
 app.use('/', webRoutes);
 
-app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${hostname}:${port}`);
 });
