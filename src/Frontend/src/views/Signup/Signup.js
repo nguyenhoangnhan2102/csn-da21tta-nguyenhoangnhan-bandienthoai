f12807c9a849e075604ee983e9c1f0c74cebf1eb
unknown <nhnhan2102@gmail.com>
2024-06-05 23:39:58 +0700

    update

--- src/Frontend/src/views/Signup/Signup.js
+++ src/Frontend/src/views/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import './Signup.scss';
 import { NavLink } from 'react-router-dom';
 import Nav from '../Nav/Nav';
